0d23a2f1c090785ca818fd0391114edb17f0a242
bistrodelsantuario-beep <bistrodelsantuario@gmail.com>
2026-09-24 18:22:23 +0200

    Configura Supabase Magazzino DS

---
+++ config.js
@@ -0,0 +1,4 @@
+window.MAGAZZINO_SUPABASE_URL = "https://fjblofhipppjrdimdkcn.supabase.co";
+window.MAGAZZINO_SUPABASE_KEY = "sb_publishable_LgaY4b3KdEE8f_CG7ywA4Q_7d15FZ1a";
+
+window.MAGAZZINO_VAPID_PUBLIC = "BFKHTJ4q6j7IT2rQUEGck8cs0siIrjKOzMdJRoqswxhc-J9dpffJOsjsms_pYl7UwPE0LFZYSEf5V6UyF2oif4A";
